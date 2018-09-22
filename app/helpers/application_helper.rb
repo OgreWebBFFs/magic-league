f48bd908b811d428a1ece55fdd80b2eb7e36d1ef
Zack Brown <zack.t.brown@gmail.com>
2018-09-22 17:39:35 -0400

    integrate @patrickwroach's view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  BGARR = ["bg-Unbridled-Growth.jpg", "bg-Llanowar-Envoy-Dominaria-MtG-Art.jpg", "bg-Ancient-Hellkite-MtG-Art-1024x640.jpg", "bg-Slinn-Voda-the-Rising-Deep-Dominaria-MtG-Art.jpg", "bg-Traxos-Scourge-of-Kroog-Dominaria-MtG-Art.jpg", "bg-Mana-Charged-Dragon-MtG-Art.jpg"]
+
+  def get_random_bg_image
+    image_path BGARR[rand(0..5)]
+  end
 end
