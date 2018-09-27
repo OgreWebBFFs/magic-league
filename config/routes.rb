2214666d7b0bd803ba83bb634fad87c754ae3df2
roachp3 <roachp3@nationwide.com>
2018-09-27 13:24:52 -0400

    Moving Nav/Footer to app...erb, adding static dashboard page
    Current issue with match logger

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root "home#index"
   resources :matches, only: [:create]
+  resources :dashboard, only: [:index]
 end
