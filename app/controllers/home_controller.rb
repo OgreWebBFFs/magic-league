9b37ed7aa43a439c6386a4c30713da5392065dd3
Zack Brown <zack.t.brown@gmail.com>
2018-09-29 09:40:54 -0400

    use rankings object to show user rankings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     matches = Match.all
 
     RankingEngine.new(@users, matches).generate_rankings
-    @sorted_users = @users.sort_by(&:ranking).reverse
+    @rankings = @users.sort_by(&:ranking).reverse
   end
 end
