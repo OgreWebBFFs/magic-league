d2882c5f532f35df63e5d353bb6a76e427ddc283
Zack Brown <zack.t.brown@gmail.com>
2018-10-02 06:27:20 -0400

    associate collection with user at sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+    :recoverable, :rememberable, :validatable, :trackable,
+    :omniauthable, omniauth_providers: [:google_oauth2]
+
+  before_create :add_collection
 
   attr_accessor :ranking
 
@@ -27,12 +29,18 @@ class User < ApplicationRecord
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
-        user = User.create(name: data['name'],
-           email: data['email'],
-           password: Devise.friendly_token[0,20]
-        )
+      user = User.create(name: data['name'],
+                         email: data['email'],
+                         password: Devise.friendly_token[0,20]
+                        )
     end
     user
-end
+  end
+
+  private
+
+  def add_collection
+    self.collection = Collection.new
+  end
 
 end
