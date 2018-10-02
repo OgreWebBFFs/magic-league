7139f4d855961db975f30e450675e8e08491e6ad
Zack Brown <zack.t.brown@gmail.com>
2018-10-02 06:35:47 -0400

    add collections to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@
 
 users = []
 users << User.find_or_create_by(email: "patrickwroach@gmail.com", name: "Pat Roach")
-users << User.find_or_create_by(email: "zack@gmail.com", name: "Zack Brown")
+users << User.find_or_create_by(email: "zack.t.brown@gmail.com", name: "Zack Brown")
 users << User.find_or_create_by(email: "mike@gmail.com", name: "Mike Stempler")
 users << User.find_or_create_by(email: "joe@gmail.com", name: "Joe Handzel")
 users << User.find_or_create_by(email: "dustin@gmail.com", name: "Dustin Perzanowski")
 users << User.find_or_create_by(email: "ryan@gmail.com", name: "Ryan Branch")
 
-users.each {|user| user.password = "12345678"; user.save!}
+users.each {|user| user.collection = Collection.new; user.password = "12345678"; user.save!}
