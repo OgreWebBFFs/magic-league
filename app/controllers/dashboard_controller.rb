fc263d4841307ece1b1236b90738ac334a9d617d
Dustin Perzanowski <perzanowski.4@osu.edu>
2018-10-03 09:25:59 -0400

    show current user name and edit link on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
     def index
       @cards = current_user.cards.order(:name).group(:id)
       @count = @cards.count
+      @user = current_user
     end
   end
-
