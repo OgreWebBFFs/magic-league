aecedd8350480cec4487cc9041239c88aaa062b6
Zack Brown <zack.t.brown@gmail.com>
2018-10-06 15:15:30 -0400

    DRY it up a little

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,57 @@
 //= require turbolinks
 //= require jquery3
 //= require_tree .
+var token = $("meta[name='csrf-token']").attr("content");
+
+var cardTemplate = function(card) {
+    return `
+      <div class="card-wrapper">
+        <div class="wishlist-checkbox">
+                <input class="toggle-wishlist" wish-list-id="${search('id', card.id, wishlist) === undefined ? -1 : search('id', card.id, wishlist).id}" data-id="${card.id}" id="checkbox-for-${card.id}" type="checkbox" ${search('id', card.id, wishlist) === undefined ? '' : 'checked' }>
+                <label for="checkbox-for-${card.id}"></label>
+            </div>
+      <a class="card-grid-item" href="/cards/${card.id}">
+        <img class="card" alt="${card.name}" title="${card.name}" src="${card.image_url}">
+      </a>
+      <div class="card-count-component">
+        <span>
+          <button type="button" class="remove-card btn" data-id="${card.id}"><i class="material-icons">indeterminate_check_box</i></button>
+          <input type="number" id="${card.id}-count" data-id="${card.id}" class="browser-default" value="${cardCount(card.id)}", min="0">
+          <button type="button" class="add-card btn" data-id="${card.id}"><i class="material-icons">add_box</i></button>
+        </span>
+      </div>
+        `
+  };
+
+  var debounce = function(func, delay) {
+    var inDebounce;
+    return function() {
+      var context = this;
+      var args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(function() {
+        func.apply(context, args);
+      }, delay);
+    }
+  }
+
+  var search = function(key, value, arr){
+    return arr.find(function(item) {
+      return item[key] == value
+    });
+  }
+
+  var xhrRequest = function(url, type, success = function(){}, data = {}, error = function(){}) {
+    $.ajax({
+      url: url,
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader("X-CSRF-Token", token)
+      },
+      data: data,
+      type: type,
+      dataType: "json",
+      error: error,
+      success: success
+    });
+  };
+
