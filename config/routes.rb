79f9c005b72877303c7d41364bd13aaf7f14038b
Zack Brown <zack.t.brown@gmail.com>
2018-10-10 16:59:46 -0400

    send mail when trade proposal is offered

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :matches, only: [:create]
   resources :dashboard, only: [:index]
   resources :rules, only: [:index]
-  resources :trades, only: [:index]
+  resources :trades, only: [:index, :create]
   resources :collections, only: [:index, :show, :edit, :update]
   resources :cards, only: [:index, :show]
   resources :ownerships, only: [:create, :destroy]
