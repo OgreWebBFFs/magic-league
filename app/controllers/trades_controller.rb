3cd905b38be579f2c92dc9962ef6bdbdc2c53b0e
Zack Brown <zack.t.brown@gmail.com>
2018-10-10 16:59:46 -0400

    send mail when trade proposal is offered

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -2,5 +2,20 @@ class TradesController < ApplicationController
   def index
 
   end
+
+  def create
+    card = Card.find(trades_params[:card_id])
+    card.users.select{|a| a.id != current_user.id }.map{|a| a.id}.each do |id|
+      UserMailer.trade_proposal_email(current_user.id, id, card.id).deliver_later
+    end
+    flash[:success] = "Trade emails successfully sent!"
+    redirect_to trades_path
+  end
+
+  private
+
+  def trades_params
+    params.require(:trade).permit(:card_id)
+  end
 end
 
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+  def trade_proposal_email(from_user_id, to_user_id, card_id)
+    @to_user = User.find(to_user_id)
+    @from_user = User.find(from_user_id)
+    @card = Card.find(card_id)
+    mail(to: @to_user.email, from: "#{@from_user.name} <#{@from_user.email}>", subject: "#{@from_user.name} wants your #{@card.name}")
+  end
+end
