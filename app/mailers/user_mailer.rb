d8dfa147b269a3398e9e3b70fba7e2a7f25a9ec4
Zack Brown <zack.t.brown@gmail.com>
2018-10-15 16:48:19 -0400

    do reply_to instead of from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
     @to_user = User.find(to_user_id)
     @from_user = User.find(from_user_id)
     @card = Card.find(card_id)
-    mail(to: @to_user.email, from: "#{@from_user.name} <#{@from_user.email}>", cc: @from_user.email, subject: "#{@from_user.name} wants your #{@card.name}")
+    mail(to: @to_user.email, reply_to: @from_user.email, cc: @from_user.email, subject: "#{@from_user.name} wants your #{@card.name}")
   end
 end
