466e30c006e0f9f800b1b3935a402e791de1fce2
Dustin Perzanowski <perzanowski.4@osu.edu>
2018-10-16 14:11:41 -0400

    use custom default image for gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
   def gravatar(user = nil, size = 207)
-    image_tag "#{user&.gravatar_path}?s=#{size}"
+    default_image = URI.encode(image_url('user.png'))
+    image_tag "#{user&.gravatar_path}?s=#{size}&d=#{default_image}"
   end
 end
