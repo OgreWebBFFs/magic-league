d6e612a89eec0f79ba077f3cc23f7684abbb14e4
Dustin Perzanowski <perzanowski.4@osu.edu>
2018-10-16 14:19:32 -0400

    provide default gravatar only on non-development environments

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,10 @@
 module UsersHelper
   def gravatar(user = nil, size = 207)
     default_image = URI.encode(image_url('user.png'))
-    image_tag "#{user&.gravatar_path}?s=#{size}&d=#{default_image}"
+
+    image_path = "#{user&.gravatar_path}?s=#{size}"
+    image_path = image_path + "&d=#{default_image}" unless Rails.env == 'development'
+
+    image_tag image_path
   end
 end
