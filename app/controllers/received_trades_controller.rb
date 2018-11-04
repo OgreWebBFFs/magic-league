e5526fcd50d097d8fccedd52b0a9112cb9a1a4df
Joe Handzel <handzel.1@osu.edu>
2018-11-04 08:21:15 -0500

    Received Trades - controller - better way of setting a value to update

--- app/controllers/received_trades_controller.rb
+++ app/controllers/received_trades_controller.rb
@@ -3,8 +3,7 @@ class ReceivedTradesController < ApplicationController
   def create
     trade = @user.received_trades.find_or_initialize_by(rarity: params[:rarity])
     fallback_value = trade.num_received ? trade.num_received : 0
-    trade.num_received = params[:num_received]
-    if trade.save
+    if trade.update(num_received: params[:num_received])
       render json: { success: 'true' }, status: 200
     else
       render json: { success: 'false', fallback_value: fallback_value }, status: 200
