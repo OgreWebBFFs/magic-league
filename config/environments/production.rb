5c519fd17fe89ddf30cab28e4946997675c8463a
Zack Brown <zack.t.brown@gmail.com>
2018-11-11 13:22:35 -0500

    set host to mtgleague-xyz

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mtg-league_#{Rails.env}"
+  #
+
+  config.action_mailer.default_url_options = { :host => 'mtgleague.xyz' }
 
   config.action_mailer.perform_caching = false
 
