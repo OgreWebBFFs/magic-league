561b0479794f951be73eb6708ce0607a0fd959c9
Zack Brown <zack.t.brown@gmail.com>
2018-11-19 12:47:28 -0500

    only display win/loss and rankings for this month on root page

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,6 @@
 class Match < ApplicationRecord
+  scope :played_during_month, ->(date) { where(played_at: date.beginning_of_month..date.end_of_month) }
+
   belongs_to :winner, class_name: 'User', foreign_key: 'winner_id'
   belongs_to :loser, class_name: 'User', foreign_key: 'loser_id'
 
