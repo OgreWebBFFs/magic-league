7102298196b54ca2f782ba4616f61217bde62e54
Zack Brown <zack.t.brown@gmail.com>
2018-11-19 12:47:28 -0500

    only display win/loss and rankings for this month on root page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    matches = Match.all
+    @matches = Match.played_during_month(DateTime.now)
 
-    RankingEngine.new(@users, matches).generate_rankings
+    RankingEngine.new(@users, @matches).generate_rankings
     @rankings = @users.sort_by(&:ranking).reverse
   end
 end
