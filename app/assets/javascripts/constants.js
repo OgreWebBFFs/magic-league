ca009bc34ab78831f58a6877881afc786e99b706
Joe Handzel <handzel.1@osu.edu>
2019-01-09 20:16:05 -0500

    Card Search - Trade and Collection   - Updated placeholder text on input   - Added helper text when there are less than the minimum number of characters, and would normally send a request   - Added helper text when there are no results

--- app/assets/javascripts/constants.js
+++ app/assets/javascripts/constants.js
@@ -1,7 +1,11 @@
 var Constants = (function() {
   const min_query_length = 3;
+  const card_search_placeholder = `Input at least ${min_query_length} characters to search`;
+  const card_search_no_results = "There were no results found for this search";
 
   return {
     min_query_length: min_query_length,
+    card_search_placeholder: card_search_placeholder,
+    card_search_no_results: card_search_no_results,
   }
 })();
