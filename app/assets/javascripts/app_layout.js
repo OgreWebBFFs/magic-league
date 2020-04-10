12bb659af4a92816a2b213dcab49dc4cebe1d121
Pat <proach@gap.com>
2020-04-10 14:30:10 -0400

    Instituting BEM for top nav, and sectioning out its styles

--- app/assets/javascripts/app_layout.js
+++ app/assets/javascripts/app_layout.js
@@ -33,22 +33,24 @@ var AppLayout = (function() {
     setMatchDateAndTime();
   }
 
-  let handleWindowClick = function(event) {
+ /*  let handleWindowClick = function(event) {
     if (!event.target.matches('.drop-down-toggle')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
+      console.log("this is also happenieng")
+      var dropdowns = document.querySelectorAll([class$="_drop-down-content"]);
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
+        console.log("this is also happenieng")
         if (openDropdown.classList.contains('active')) {
           openDropdown.classList.remove('active');
         }
       }
     }
-  }
+  } */
 
   // Exposed functions start here
   let closeOtherDropDown = function() {
-    var dropDownMenus = document.getElementById("nav").querySelectorAll(".dropdown-content");            
+    var dropDownMenus = document.getElementById("top-nav").querySelectorAll(".nav_menu_dropdown-content");            
     for (var i = 0; i < dropDownMenus.length; i++) {
       dropDownMenus[i].classList.remove("active");
     }
