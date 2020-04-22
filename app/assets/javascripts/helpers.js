d12723a63fa24ee6418e6de366c916fc3937a4fb
patrickwroach <patrickwroach@gmail.com>
2020-04-22 00:01:44 -0400

    updateing toggle and remove active class to be class agnostic

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -1,9 +1,19 @@
-var Helpers = (function() {
+var Helpers = (function () {
 
-  let toggleElementById = function(elementId) {
-    console.log('toggle fired');
+  let toggleElementById = (elementId, siblingsToClose) => {
+    if (siblingsToClose) {
+      console.log('sib to close yes')
+      unToggleSiblingElements(siblingsToClose);
+    }
     let target = document.getElementById(elementId);
     target.classList.toggle("active");
+  }
+
+  let unToggleSiblingElements = (target) => {
+    var siblingClass = document.querySelectorAll("." + target);
+    for (var i = 0; i < siblingClass.length; i++) {
+      siblingClass[i].classList.remove('active');
+    }
   }
 
   return {
