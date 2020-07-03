ba5e8af6a9d1133852ff3a81d95b11f984191d9f
Joe Handzel <joseph.handzel@gmail.com>
2020-07-03 14:23:01 -0400

    Card - show: added wishlist button functionality

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,5 +3,6 @@ class Card < ApplicationRecord
   has_many :collections, through: :ownerships 
   has_many :users, -> { distinct },  through: :collections 
   has_many :tradables
+  has_many :wishes
   scope :query, ->(string) { where(arel_table[:name].matches("%#{sanitize_sql_like string}%")) }
 end
