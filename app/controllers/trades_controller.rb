0c7269769261232cc7965996b99f0833d93f96b0
Joe Handzel <joseph.handzel@gmail.com>
2020-07-04 21:09:29 -0400

    Card - show: Individual trade buttons now functional trades_controller: now supports sending an email to a specific user

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -5,17 +5,24 @@ class TradesController < ApplicationController
 
   def create
     card = Card.find(trades_params[:card_id])
-    card.users.select{|a| a.id != current_user.id }.map{|a| a.id}.each do |id|
-      UserMailer.trade_proposal_email(current_user.id, id, card.id).deliver_later
+    user_id = trades_params[:user_id]
+    if (user_id) 
+      UserMailer.trade_proposal_email(current_user.id, user_id, card.id).deliver_later
+      flash[:success] = "Trade email successfully sent!"
+    else
+      card.users.select{|a| a.id != current_user.id }.map{|a| a.id}.each do |id|
+        UserMailer.trade_proposal_email(current_user.id, id, card.id).deliver_later
+      end
+      flash[:success] = "Trade emails successfully sent!"
     end
-    flash[:success] = "Trade email(s) successfully sent!"
+    
     redirect_to request.referer
   end
 
   private
 
   def trades_params
-    params.require(:trade).permit(:card_id)
+    params.require(:trade).permit(:card_id, :user_id)
   end
 end
 
