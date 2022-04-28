f15b0a0fca64c4672915c1c65538586f25337bfe
patrickwroach <patrickwroach@gmail.com>
2022-04-28 16:00:16 -0400

    feat: hide reveal rankigns

--- app/javascript/components/Rankings/index.js
+++ app/javascript/components/Rankings/index.js
@@ -1,13 +1,14 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import classNames from "classnames";
 
 import Button from "../Button";
 
 
 const Rankings = ({rankedPlayers, unrankedPlayers}) => {
-    useEffect()={
-        
-    }
+    const [showRankings, setShowRankings] = useState(true)
+
+    const rankingsRef= useRef(null);
+    const rankingsBorderRef =  useRef(null);
 
     const getPlayers = (playerArr, isRanked=true) => playerArr.map((user, i)=>{
        const  {name, id, ranking, wins=0, losses=0} = user.table   
@@ -35,16 +36,22 @@ const Rankings = ({rankedPlayers, unrankedPlayers}) => {
         )
     })
 
-
+    const toggleRankingsVisible = () => {
+        const dateForm =  document.querySelector(".rankings__date_form");
+        dateForm.classList.toggle("rankings__date_form--hidden");
+        setShowRankings(!showRankings)
+    }
 
     return (
-
-   <div className="rankings container">
-        <h1 className="rankings__title">Rankings</h1>
-      
-        <div className="rankings__wrapper">
+    <>
+   <div ref={rankingsRef} className={classNames("rankings", "container", {"rankings--hidden":!showRankings})}>      
+        <Button className={"button--ghost rankings__title"} onClick={()=>{toggleRankingsVisible()}} >
+            Rankings
+        </Button>
+    
+        <div className={"rankings__wrapper"}>
        
-            <svg  className="rankings__border" xmlns="http://www.w3.org/2000/svg">
+            <svg ref={rankingsBorderRef} className="rankings__border" xmlns="http://www.w3.org/2000/svg">
                 <rect className="rankings__border-shape1"/>
             </svg>
           
@@ -53,18 +60,22 @@ const Rankings = ({rankedPlayers, unrankedPlayers}) => {
             <div className="rankings__vert-centerer">
                 <div className="rankings__player-listing">
                 <div id="ranked-players" className="rankings__player-bucket">
-                        {getPlayers(rankedPlayers)}
+                    {getPlayers(rankedPlayers)}
                 </div>
                 <hr className="rankings__divider"/>
                 <div id="unranked-players" className="rankings__player-bucket">
-                     {getPlayers(unrankedPlayers, false)}
+                    {getPlayers(unrankedPlayers, false)}
                 </div>
                 </div>
             </div>
             </div>
-         
+
         </div>
+     
     </div> 
+    { !showRankings &&
+    <Button className={classNames("rankings__toggle-visbility-button", "button--inverse")} onClick={()=>{toggleRankingsVisible()}}> <i className="fas fa-list"></i></Button>}
+    </>
     )
 }
 
