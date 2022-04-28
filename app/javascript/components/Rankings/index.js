37a830438edcc5fc7278affad798cc6847275d0c
patrickwroach <patrickwroach@gmail.com>
2022-04-28 12:36:47 -0400

    fix: cleaning console logs and unused styles

--- app/javascript/components/Rankings/index.js
+++ app/javascript/components/Rankings/index.js
@@ -5,24 +5,28 @@ import Button from "../Button";
 
 
 const Rankings = ({rankedPlayers, unrankedPlayers}) => {
+    useEffect()={
+        
+    }
+
     const getPlayers = (playerArr, isRanked=true) => playerArr.map((user, i)=>{
        const  {name, id, ranking, wins=0, losses=0} = user.table   
         return (
-            <Button className={classNames("rankings__user-button", {"rankings__user-button--unranked": !isRanked})} href={`/users/${id}`}>
-               { isRanked && <div class="rankings__player-position">
+            <Button key={`${name}-${id}`}className={classNames("rankings__user-button", {"rankings__user-button--unranked": !isRanked})} href={`/users/${id}`}>
+               { isRanked && <div className="rankings__player-position">
                     {i+1}
                 </div>
                 }
-                <div class="rankings__player">
+                <div className="rankings__player">
                     <div className={classNames("rankings__player-name", {"rankings__player-name--unranked": !isRanked})}>
                         {name}
                     </div>
-                    <div class="rankings__player-stats">
-                   { isRanked &&  <div class="rankings__player-elo">
+                    <div className="rankings__player-stats">
+                   { isRanked &&  <div className="rankings__player-elo">
                         {ranking}
                     </div>
                    }
-                    <div class="rankings__player-record">
+                    <div className="rankings__player-record">
                             {wins}-{losses}
                     </div>
                     </div>
@@ -35,24 +39,24 @@ const Rankings = ({rankedPlayers, unrankedPlayers}) => {
 
     return (
 
-   <div class="rankings container">
-        <h1 class="rankings__title">Rankings</h1>
+   <div className="rankings container">
+        <h1 className="rankings__title">Rankings</h1>
       
-        <div class="rankings__wrapper">
+        <div className="rankings__wrapper">
        
-            <svg  class="rankings__border" xmlns="http://www.w3.org/2000/svg">
-                <rect class="rankings__border-shape1"/>
+            <svg  className="rankings__border" xmlns="http://www.w3.org/2000/svg">
+                <rect className="rankings__border-shape1"/>
             </svg>
           
             
-            <div class="rankings__scroll-catcher">
-            <div class="rankings__vert-centerer">
-                <div class="rankings__player-listing">
-                <div id="ranked-players" class="rankings__player-bucket">
+            <div className="rankings__scroll-catcher">
+            <div className="rankings__vert-centerer">
+                <div className="rankings__player-listing">
+                <div id="ranked-players" className="rankings__player-bucket">
                         {getPlayers(rankedPlayers)}
                 </div>
-                <hr class="rankings__divider"/>
-                <div id="unranked-players" class="rankings__player-bucket">
+                <hr className="rankings__divider"/>
+                <div id="unranked-players" className="rankings__player-bucket">
                      {getPlayers(unrankedPlayers, false)}
                 </div>
                 </div>
