66584f4396b2d473adb891b4d493f679293b32e7
patrickwroach <patrickwroach@gmail.com>
2022-05-02 15:24:37 -0400

    fix styles dashboard

--- app/javascript/components/TradeTracker/index.jsx
+++ app/javascript/components/TradeTracker/index.jsx
@@ -2,30 +2,27 @@ import React from 'react';
 import { Table, Row, Cell } from '../Table';
 import TradeTrackerInput from './TradeTrackerInput';
 
-const OneThirdCell = ({ children }) => (
-  <Cell size={"33.33%"}>{children}</Cell>
-)
 
 const TradeTracker = ({ trades_data, user_id, current_user_id}) => {
-  return (<div class="dashboard_profile_trades">
+  return (<div class="dashboard-profile__trades">
     <Table>
       <Row isHeading={true}>
-        <OneThirdCell>Trades</OneThirdCell>
-        <OneThirdCell>Received</OneThirdCell>
-        <OneThirdCell>Allowed</OneThirdCell>
+        <Cell>Trades</Cell>
+        <Cell>Received</Cell>
+        <Cell>Allowed</Cell>
       </Row>
       {trades_data.map(trade => (
         <Row>
-          <OneThirdCell>{trade.rarity.charAt(0).toUpperCase() + trade.rarity.slice(1)}</OneThirdCell>
-          <OneThirdCell>
+          <Cell>{trade.rarity.charAt(0).toUpperCase() + trade.rarity.slice(1)}</Cell>
+          <Cell>
             <TradeTrackerInput
               {...trade}
               isOwner={user_id === current_user_id}
               currentUserId={current_user_id}/>
-          </OneThirdCell>
-          <OneThirdCell>
+          </Cell>
+          <Cell>
             {trade.num_allowed}
-          </OneThirdCell>
+          </Cell>
         </Row>
       ))}
     </Table>
