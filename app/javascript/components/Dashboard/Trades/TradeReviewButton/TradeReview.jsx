125a60f37f7ed4123f87a66af459d942c7e81fca
Brandon Seich <blseich@gmail.com>
2022-05-09 16:52:50 -0400

    refactor card review table for reusability

--- app/javascript/components/Dashboard/Trades/TradeReviewButton/TradeReview.jsx
+++ app/javascript/components/Dashboard/Trades/TradeReviewButton/TradeReview.jsx
@@ -3,9 +3,10 @@ import classNames from "classnames";
 import Button from "../../../Button";
 import { Row, Table, Cell } from "../../../Table";
 import postTradeReview from "./post-trade-review";
+import CardReviewTable from "./CardReviewTable";
 
-const ReceiveCell = ({ children }) => <Cell className="trade-review-modal__cell receive">{children}</Cell>
-const GiveCell = ({ children }) => <Cell className="trade-review-modal__cell give">{children}</Cell>
+const ReceiveCell = ({ children }) => <Cell className="trade-review-modal__cell card-cell">{children}</Cell>
+const GiveCell = ({ children }) => <Cell className="trade-review-modal__cell card-cell">{children}</Cell>
 const RarityCell = ({ rarity }) => <Cell className={classNames("trade-review-modal__cell-rarity", rarity)}>{rarity.substr(0,1).toUpperCase()}</Cell>
 const TradeReview = ({ trade }) => {
   return (
@@ -13,30 +14,14 @@ const TradeReview = ({ trade }) => {
       <h3>Reviewing Trade</h3>
       <p><span style={{fontWeight: "900"}}>{trade.from.name}</span> has proposed the following trade:</p>
       <div className={"trade-review-modal__contents"}>
-        <Table className="trade-review-modal__table">
-          <Row isHeading={true}>
-            <ReceiveCell>You Receive</ReceiveCell>
-          </Row>
-          {trade.from.cards.map(card => (
-            <Row key={`review_row_from#${card.id}`}>
-              <Cell><i className="fas fa-arrow-left"></i></Cell>
-              <ReceiveCell>{card.name}</ReceiveCell>
-              <RarityCell rarity={card.rarity} />
-            </Row>
-          ))}
-        </Table>
-        <Table className="trade-review-modal__table">
-          <Row isHeading={true}>
-            <GiveCell>{`${trade.from.name} Receives`}</GiveCell>
-          </Row>
-          {trade.to.cards.map(card => (
-            <Row key={`review_row_to#${card.id}`}>
-              <RarityCell rarity={card.rarity} />
-              <GiveCell>{card.name}</GiveCell>
-              <Cell><i class="fas fa-arrow-right"></i></Cell>
-            </Row>
-          ))}
-        </Table>
+        <CardReviewTable
+          cards={trade.from.cards}
+          header={"You Receive"}
+          arrow={<i className="fas fa-arrow-left"></i>} />
+        <CardReviewTable
+          cards={trade.to.cards}
+          header={`${trade.to.name} Receives`}
+          arrow={<i className="fas fa-arrow-right"></i>} />
       </div>
       <div className="trade-review-modal__actions">
         <Button onClick={() => postTradeReview(trade, "approved")} className="trade-review-modal__action-button approve">Accept</Button>
