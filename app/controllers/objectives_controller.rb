3da18dacf7252bb92ef0c785f799d28b3fe1ac96
Brandon Seich <blseich@gmail.com>
2022-06-04 21:55:03 -0400

    setup db and basic modifying api for objectives

---
+++ app/controllers/objectives_controller.rb
@@ -0,0 +1,20 @@
+class ObjectivesController < ApplicationController
+  def create
+    Objective.create(description: params[:description], value: params[:value])
+  end
+
+  def update
+    objective = Objective.find(params[:id])
+    objective.update(description: params[:description], value: params[:value])
+  end
+
+  def destroy
+    objective = Objective.find(params[:id])
+    objective.destroy
+  end
+
+  def index
+    objective = Objective.all
+    render json: objective.to_json()
+  end
+end
