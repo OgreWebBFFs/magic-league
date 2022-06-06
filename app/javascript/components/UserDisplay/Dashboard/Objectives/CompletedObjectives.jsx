3032a116d69545500bec1b2eb2029a8faee7c091
Brandon Seich <blseich@gmail.com>
2022-06-06 00:19:43 -0400

    triumphs sounds cooler

--- app/javascript/components/UserDisplay/Dashboard/Objectives/CompletedObjectives.jsx
+++ app/javascript/components/UserDisplay/Dashboard/Objectives/CompletedObjectives.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Table, Row, Cell } from '../../../Table';
 
 const KeepCell = ({ children }) => <Cell className="dashboard__active-objects--keep-cell">{children}</Cell>;
-const DescriptionCell = ({ children }) => <Cell isPriority className="dashboard__active-objects--description-cell">{children}</Cell>;
+const DescriptionCell = ({ children }) => <Cell className="dashboard__active-objects--description-cell">{children}</Cell>;
 
 const CompletedObjectives = ({ completedObjectives }) => (
   <Table>
     <Row isHeading>
       <KeepCell>Date</KeepCell>
-      <DescriptionCell>Quest</DescriptionCell>
+      <DescriptionCell>🏆 Triumphs 🏆</DescriptionCell>
     </Row>
     {completedObjectives.map(({ data: { attributes: { description, completed_at } } }) => (
       <Row>
