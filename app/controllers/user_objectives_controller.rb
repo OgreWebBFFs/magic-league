51541ff4946a56ef3a9fded9b14c0e167c74d9c2
Brandon Seich <blseich@gmail.com>
2022-06-06 08:39:18 -0400

    refactor complete and reroll logic

--- app/controllers/user_objectives_controller.rb
+++ app/controllers/user_objectives_controller.rb
@@ -7,10 +7,9 @@ class UserObjectivesController < ApplicationController
 
   def complete
     user_objective = UserObjective.find(params[:id])
-    user_objective.update(completed_at: Time.now)
-    all_users_objectives = user_objective.user.user_objectives.pluck(:objective_id)
-    new_objective = Objective.where.not(id: all_users_objectives).sample
-    UserObjective.create(objective: new_objective, user: user_objective.user, assigned_at: Time.now)
+    user_objective.complete
+    user_objective.reroll
+    
     render json: { status: 'success' }
   end
 end
--- app/models/user_objective.rb
+++ app/models/user_objective.rb
@@ -3,4 +3,18 @@ class UserObjective < ApplicationRecord
   belongs_to :objective
 
   validates :user_id, uniqueness: { scope: :objective_id, message: 'A user can only be assigned objective that they are not currently assigned or have not previously completed' }
+
+  def complete
+    self.update(completed_at: Time.now)
+  end
+
+  def reroll
+    user = self.user
+    all_users_objectives = user.user_objectives.pluck(:objective_id)
+    new_objective = Objective.where.not(id: all_users_objectives).sample
+    UserObjective.create(objective: new_objective, user: user, assigned_at: Time.now)
+    if (self.completed_at == nil)
+      self.destroy
+    end
+  end
 end
