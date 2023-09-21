3fd6311e7cbee3f58947cad79ac5c61ae68680a8
Brandon Seich <blseich@gmail.com>
2023-09-21 02:50:09 -0400

    resize images; start logic for unavailable indicator

--- app/helpers/draffle_img_helper.rb
+++ app/helpers/draffle_img_helper.rb
@@ -10,6 +10,7 @@ module DraffleImgHelper
 
   def build_prize_pool_img prizes
     compiled_img = ImageList.new
+    rows = 0
     prizes.each_slice(ROW_LENGTH) do |prize_row|
       img_row = ImageList.new
       prize_row.each do |prize|
@@ -19,10 +20,20 @@ module DraffleImgHelper
           foil = Image.read("./app/javascript/images/foil_indicator.png").first
           img = img.composite(foil, CenterGravity, OverCompositeOp)
         end
+        img = img.resize_to_fit(373, 520)
         img_row.push(img)
+        rows = rows + 1
       end
       compiled_img.push(img_row.append(false))
     end
-    compiled_img.append(true).write("test.png")
+    compiled_img = compiled_img.append(true)
+    gc = Draw.new
+    gc.stroke '#5c1009'
+    gc.stroke_width 10
+    gc.fill 'black'
+    gc.fill_opacity 0.8
+    gc.rectangle 372, 520, 744, 1040
+    gc.draw(compiled_img)
+    compiled_img.write("test.png")
   end
 end
