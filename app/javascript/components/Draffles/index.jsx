52de9e47c978bac7e4efd1c9a7bbb70782a44a8e
Brandon Seich <blseich@gmail.com>
2023-10-10 21:54:57 -0400

    start to style the draffle portal

--- app/javascript/components/Draffles/index.jsx
+++ app/javascript/components/Draffles/index.jsx
@@ -5,20 +5,30 @@ import Button from '../Button';
 const Draffles = ({ draffles, isAdmin }) => (
   <>
     <h2>Draffles</h2>
-    <Table>
-      <Row isHeading>
-        <Cell>Date</Cell>
-        <Cell>Name</Cell>
-        <Cell>Status</Cell>
-      </Row>
-      {draffles.map(({ table: draffle }) => (
-        <Row key={draffle.id}>
-          <Cell>{draffle.date}</Cell>
-          <Cell>{draffle.name}</Cell>
-          <Cell>{draffle.status}</Cell>
+    {draffles.length > 0 ? (
+
+      <Table className="draffle-table">
+        <Row isHeading>
+          <Cell className="date-cell">Date</Cell>
+          <Cell className="name-cell">Name</Cell>
+          <Cell className="status-cell">Status</Cell>
+          <Cell className="action-cell" />
         </Row>
-      ))}
-    </Table>
+        {draffles.map(({ table: draffle }) => (
+          <Row key={draffle.id}>
+            <Cell className="date-cell">{draffle.date}</Cell>
+            <Cell className="name-cell">{draffle.name}</Cell>
+            <Cell className="status-cell">{draffle.status}</Cell>
+            <Cell className="action-cell">
+              <a href={`/draffles/${draffle.id}`}>
+                <i className="fas fa-eye" />
+                View
+              </a>
+            </Cell>
+          </Row>
+        ))}
+      </Table>
+    ) : <p>No Draffles Yet...</p>}
     {isAdmin && (
     <form action="/draffles" method="post">
       <Button name="" value="" type="submit">Create New Draffle</Button>
