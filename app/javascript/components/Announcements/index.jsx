54cc190c248a3425d4dc90962dcc0d9b08c57904
Brandon Seich <blseich@gmail.com>
2024-01-28 17:37:38 -0500

    add "new!" message to announcements that are less than 10 days old

--- app/javascript/components/Announcements/index.jsx
+++ app/javascript/components/Announcements/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { toHTML } from 'discord-markdown';
 
+const isNew = (age) => age.includes('minute') || age.includes('hour') || (
+  age.includes('day') && parseInt(age.match(/\d+/ig), 10) < 10
+);
+
 const Announcements = ({ messages }) => (
     <>
       <h1>Recent Announcements</h1>
       {messages.map(({ heading, sub_heading: subHeading, author, age, link, id }) => (
         <div className="announcement" key={id}>
+          {isNew(age) && <div className="announcement__new">NEW!</div>}
           <div
             // eslint-disable-next-line react/no-danger
             className="announcement__heading"
