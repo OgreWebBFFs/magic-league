e7f2a7ac059fe980daf492aa1712faf80b4d8cfb
patrickwroach <patrickwroach@gmail.com>
2024-02-03 00:17:33 -0500

    style: reorganize css variables neutral and inverse

--- app/javascript/components/UserDisplay/Dashboard/ActionBar/ViewToggle.jsx
+++ app/javascript/components/UserDisplay/Dashboard/ActionBar/ViewToggle.jsx
@@ -4,10 +4,10 @@ import Button from '../../../Button';
 
 const ViewToggle = ({ isListView, setIsListView }) => (
   <div className="dashboard__card-view-toggles">
-    <Button id="collection-table-toggle" className={classNames('dashboard__card-view-toggle', { 'button--inactive': !isListView })} onClick={() => setIsListView(true)}>
+    <Button id="collection-table-toggle" className='dashboard__card-view-toggle button--secondary' onClick={() => setIsListView(true)}>
       <i className="fas fa-list" />
     </Button>
-    <Button id="collection-grid-toggle" className={classNames('dashboard__card-view-toggle', { 'button--inactive': isListView })} onClick={() => setIsListView(false)}>
+    <Button id="collection-grid-toggle" className='dashboard__card-view-toggle  button--secondary'  onClick={() => setIsListView(false)}>
       <i className="fas fa-th-large" />
     </Button>
   </div>
