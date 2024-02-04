83138a2c80a637a90f54fd33240891c517fa83d6
patrickwroach <patrickwroach@gmail.com>
2024-02-03 19:28:23 -0500

    styles: update alert

--- app/javascript/components/Alert/index.jsx
+++ app/javascript/components/Alert/index.jsx
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
+import Button from '../Button';
 
 const Alert = ({ userSignedIn, key, value }) => {
   const [show, setShow] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setShow(false), 5000);
+    setTimeout(() => setShow(false), 3000);
   });
 
   return show
     && (
     <div id="alert" className={classNames('active', 'alert', `alert-${key}`, { 'logged-out': !userSignedIn })}>
-      {value}
-      <button type="button" className="alert__close-button" onClick={() => setShow(false)}>
+      <p>{value}</p>
+      <Button type="button" className="alert__close-button button--small button--ghost" onClick={() => setShow(false)}>
         <i className="fas fa-times" />
-      </button>
+      </Button>
     </div>
     );
 };
