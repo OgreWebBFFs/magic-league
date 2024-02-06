7494bb2b90764cb94dd4f807b67ef38a5223ab35
patrickwroach <patrickwroach@gmail.com>
2024-02-06 18:33:50 -0500

    style: table css refactor

--- app/javascript/components/Table/Cell.jsx
+++ app/javascript/components/Table/Cell.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import classNames from 'classnames';
+import React from "react";
+import classNames from "classnames";
 
 const Cell = ({ className, children, isPriority }) => (
-  <div
-    className={classNames('cell', { 'cell--priority': isPriority }, className)}
-  >
-    {children}
-  </div>
+    <div className={classNames("cell", { "cell--priority": isPriority }, className)}>
+        <div className="cell__overflow-wrapper">{children}</div>
+    </div>
 );
 
 export default Cell;
--- app/javascript/components/Table/Table.jsx
+++ app/javascript/components/Table/Table.jsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import classNames from 'classnames';
+import React from "react";
+import classNames from "classnames";
 
-const Table = ({ children, className }) => (
-  <div className={classNames('table', className)}>
-    {children}
-  </div>
-);
+const Table = ({ children, className }) => <div className={classNames("table", className)}>{children}</div>;
 
 export default Table;
