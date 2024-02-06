bf9be4e704b768a12ff9c9e2a6239d8e68ea51d3
patrickwroach <patrickwroach@gmail.com>
2024-02-06 17:36:50 -0500

    fix: wishlist toggle, and nav issues

--- app/javascript/components/UserDisplay/Dashboard/Wishlist/index.jsx
+++ app/javascript/components/UserDisplay/Dashboard/Wishlist/index.jsx
@@ -1,66 +1,50 @@
-import React, { useContext } from 'react';
-import WishlistContext from '../../../../contexts/WishlistContext';
-import CardList from '../CardList';
-import {
-  CardGrid,
-  CardImageLink,
-} from '../../../CardGrid';
-import EmptyState from '../EmptyState';
-import RemoveWish from './RemoveWish';
-import { Cell, Row } from '../../../Table';
+import React, { useContext } from "react";
+import WishlistContext from "../../../../contexts/WishlistContext";
+import CardList from "../CardList";
+import { CardGrid, CardImageLink } from "../../../CardGrid";
+import EmptyState from "../EmptyState";
+import RemoveWish from "./RemoveWish";
+import { Cell, Row } from "../../../Table";
 
 const Wishlist = ({ currentUserId, user, isListView }) => {
-  const { wishlist, currentUserWishlist } = useContext(WishlistContext);
-  const isOwner = currentUserId === user.id;
-  const wishlistToRender = isOwner ? currentUserWishlist : wishlist;
-  const isEmpty = wishlistToRender.length < 1;
-  return (
-    <>
-      {isEmpty && (
-      <EmptyState
-        isOwner={isOwner}
-        user={user}
-      />
-      )}
-      {(isListView && !isEmpty) && (
-        <CardList>
-          {wishlistToRender.map((card) => (
-            <Row>
-              <Cell isPriority>
-                <a href={`/cards/${card.id}`}>{card.name}</a>
-              </Cell>
-              <Cell>
-                {isOwner && (
-                <RemoveWish
-                  user={user}
-                  card={card}
-                  classes=""
-                />
-                )}
-              </Cell>
-            </Row>
-          ))}
-        </CardList>
-      )}
-      {(!isListView && !isEmpty) && (
-        <div className="dashboard__card-grid-wrapper">
-          <CardGrid>
-            {wishlistToRender.map((card) => (
-              <>
-                {isOwner && (
-                <RemoveWish
-                  user={user}
-                  card={card}
-                  classes="button button--accent card-grid__wishlist__toggle active"
-                />
-                )}
-                <CardImageLink card={card} />
-              </>
-            ))}
-          </CardGrid>
-        </div>
-      )}
-    </>
-  );
+    const { wishlist, currentUserWishlist } = useContext(WishlistContext);
+    const isOwner = currentUserId === user.id;
+    const wishlistToRender = isOwner ? currentUserWishlist : wishlist;
+    const isEmpty = wishlistToRender.length < 1;
+    return (
+        <>
+            {isEmpty && <EmptyState isOwner={isOwner} user={user} />}
+            {isListView && !isEmpty && (
+                <CardList>
+                    {wishlistToRender.map((card) => (
+                        <Row>
+                            <Cell isPriority>
+                                <a href={`/cards/${card.id}`}>{card.name}</a>
+                            </Cell>
+                            <Cell>{isOwner && <RemoveWish user={user} card={card} classes="" />}</Cell>
+                        </Row>
+                    ))}
+                </CardList>
+            )}
+            {!isListView && !isEmpty && (
+                <div className="dashboard__card-grid-wrapper">
+                    <CardGrid>
+                        {wishlistToRender.map((card) => (
+                            <>
+                                {isOwner && (
+                                    <RemoveWish
+                                        user={user}
+                                        card={card}
+                                        classes="card-grid__wishlist-toggle active button--small"
+                                    />
+                                )}
+                                <CardImageLink card={card} />
+                            </>
+                        ))}
+                    </CardGrid>
+                </div>
+            )}
+        </>
+    );
 };
 export default Wishlist;
