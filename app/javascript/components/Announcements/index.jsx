1440d9a65ba0f6a5449d6765222bbaad2df53886
patrickwroach <patrickwroach@gmail.com>
2024-02-06 17:06:16 -0500

    chore: linitng all css files

--- app/javascript/components/Announcements/index.jsx
+++ app/javascript/components/Announcements/index.jsx
@@ -1,40 +1,41 @@
-import React from 'react';
-import { toHTML } from 'discord-markdown';
+import React from "react";
+import { toHTML } from "discord-markdown";
 
-const isNew = (age) => age.includes('minute') || age.includes('hour') || (
-  age.includes('day') && parseInt(age.match(/\d+/ig), 10) < 10
-);
+const isNew = (age) =>
+    age.includes("minute") || age.includes("hour") || (age.includes("day") && parseInt(age.match(/\d+/gi), 10) < 10);
 
 const Announcements = ({ messages }) => (
     <>
-      <h1 className="page__heading">Announcements</h1>
-      {messages.map(({ heading, sub_heading: subHeading, author, age, link, id }) => (
-        <div className="announcement" key={id}>
-          {isNew(age) && <div className="announcement__new">NEW!</div>}
-          <div
-            className="announcement__heading"
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `${toHTML(heading)}`
-            }}
-          />
-          <div className="announcement__details">
-            <div className="announcement__details--author">Sent by {author?.name || "a mysterious stranger"}</div>
-            <div className="announcement__details--age">{age}</div>
-          </div>
-          <div
-            className="announcement__subheading"
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `${toHTML(subHeading)}...`
-            }}
-          />
-          <div className="announcement__link">
-            <a href={link}>Full Message ➡</a>
-          </div>
-        </div>
-      ))}
-      </>
-  )
+        <h1 className="page__heading">Announcements</h1>
+        {messages.map(({ heading, sub_heading: subHeading, author, age, link, id }) => (
+            <div className="announcement" key={id}>
+                {isNew(age) && <div className="announcement__new">NEW!</div>}
+                <div
+                    className="announcement__heading"
+                    // eslint-disable-next-line react/no-danger
+                    dangerouslySetInnerHTML={{
+                        __html: `${toHTML(heading)}`,
+                    }}
+                />
+                <div className="announcement__details">
+                    <div className="announcement__details--author">
+                        Sent by {author?.name || "a mysterious stranger"}
+                    </div>
+                    <div className="announcement__details--age">{age}</div>
+                </div>
+                <div
+                    className="announcement__subheading"
+                    // eslint-disable-next-line react/no-danger
+                    dangerouslySetInnerHTML={{
+                        __html: `${toHTML(subHeading)}...`,
+                    }}
+                />
+                <div className="announcement__link">
+                    <a href={link}>Full Message ➡</a>
+                </div>
+            </div>
+        ))}
+    </>
+);
 
 export default Announcements;
