b10e675f71af9d33daa9d2a76b7b72583674eb6d
patrickwroach <patrickwroach@gmail.com>
2024-02-07 16:04:37 -0500

    fix: dashboard buttons set as primary

--- app/javascript/components/UserDisplay/Dashboard/ActionBar/ViewToggle.jsx
+++ app/javascript/components/UserDisplay/Dashboard/ActionBar/ViewToggle.jsx
@@ -3,10 +3,10 @@ import Button from '../../../Button';
 
 const ViewToggle = ({ setIsListView }) => (
   <div className="dashboard__card-view-toggles">
-    <Button id="collection-table-toggle" className='dashboard__card-view-toggle button--secondary' onClick={() => setIsListView(true)}>
+    <Button id="collection-table-toggle" className='dashboard__card-view-toggle' onClick={() => setIsListView(true)}>
       <i className="fas fa-list" />
     </Button>
-    <Button id="collection-grid-toggle" className='dashboard__card-view-toggle  button--secondary'  onClick={() => setIsListView(false)}>
+    <Button id="collection-grid-toggle" className='dashboard__card-view-toggle'  onClick={() => setIsListView(false)}>
       <i className="fas fa-th-large" />
     </Button>
   </div>
