2adffbaae8e4af45d5023dc7a066f96a725a583c
Joe Handzel <joseph.handzel@gmail.com>
2024-02-08 22:31:30 -0500

    home - no longer display locked users in unranked players list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     @match_date = params[:match_date] ? DateTime.new(params[:match_date][:year].to_i, params[:match_date][:month].to_i, params[:match_date][:day].to_i) : DateTime.now
 
     @ranked_players = RankingEngine.new(@match_date).generate_rankings
-    @unranked_players = User.all.select{ |u| !@ranked_players.any? { |r| r.user === u }}
+    @unranked_players = User.all.select{ |u| u.locked_at.nil? && !@ranked_players.any? { |r| r.user === u }}
   end
 end
