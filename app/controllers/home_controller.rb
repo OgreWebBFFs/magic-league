323e3a62d1cc58b06692e5766894c2f427fe2cdd
Brandon Seich <blseich@gmail.com>
2024-03-01 22:50:09 -0500

    use Time object instead of DateTime to respect rails config timezone

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @match_date = params[:match_date] ? DateTime.new(params[:match_date][:year].to_i, params[:match_date][:month].to_i, params[:match_date][:day].to_i) : DateTime.now
+    @match_date = params[:match_date] ? Time.new(params[:match_date][:year].to_i, params[:match_date][:month].to_i, params[:match_date][:day].to_i) : Time.now
 
     @ranked_players = RankingEngine.new(@match_date).generate_rankings
     @unranked_players = User.all.select{ |u| u.locked_at.nil? && !@ranked_players.any? { |r| r.user === u }}
