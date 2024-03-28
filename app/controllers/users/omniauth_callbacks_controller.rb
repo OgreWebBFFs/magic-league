6a4836a1bc54fc4b2aa3e7eeb659097198662fab
Joe Handzel <joseph.handzel@gmail.com>
2024-03-27 22:08:20 -0400

    remember_me - enable this behavior when logging in via omniauth user - add timeoutable to unlock functionality of the timeout_in config value

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  include Devise::Controllers::Rememberable
+
   def google_oauth2
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
+        remember_me(@user)
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
         sign_in_and_redirect @user, event: :authentication
       else
