880219afafc67343743bb55ff0dc409ab16f1f8b
Brandon Seich <blseich@gmail.com>
2024-05-31 14:20:09 -0400

    add support for double sided cards in draffle prints picker

--- app/javascript/components/Draffles/DraffleEdit/EditPrizes/Divider.jsx
+++ app/javascript/components/Draffles/DraffleEdit/EditPrizes/Divider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Divider = () => (
-  <div style={{ width: '2px', background: '#335f5d', flex: '0 0 2px' }} />
+  <div style={{ background: 'black', flex: '0 0 2px' }} />
 );
 
 export default Divider;
