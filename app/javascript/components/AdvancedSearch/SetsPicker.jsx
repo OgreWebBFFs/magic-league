f42eb32c6e8d580ddf3a68212e2b4a7f93a97497
patrickwroach <patrickwroach@gmail.com>
2024-08-03 17:36:33 -0400

    fix(Setpicker): add BlB and stop erroring if null set

--- app/javascript/components/AdvancedSearch/SetsPicker.jsx
+++ app/javascript/components/AdvancedSearch/SetsPicker.jsx
@@ -1,70 +1,77 @@
-import React from 'react';
+import React from "react";
 
 const SET_DETAILS = {
-  "mkm": {
-    text: "Murders at Karlov Manner",
-    symbol: 'ss ss-mkm'
-  },
-  "plst": {
-    text: "The List",
-    symbol: 'ms ms-planeswalker'
-  },
-  "spg": {
-    text: "Special Guest",
-    symbol: 'ss ss-spg'
-  },
-  "otj": {
-    text: "Outlaws of Thunder Junction",
-    symbol: 'ss ss-otj'
-  },
-  "big": {
-    text: "The Big Score",
-    symbol: 'ss ss-big',
-  },
-  "otp": {
-    text: "Breaking News",
-    symbol: 'ss ss-otp',
-  },
-  "mh3": {
-    text: "Modern Horizons 3",
-    symbol: 'ss ss-mh3',
-  },
-  "m3c": {
-    text: "Modern Horizons 3 Commander",
-    symbol: 'ss ss-m3c',
-  }
-}
+    mkm: {
+        text: "Murders at Karlov Manner",
+        symbol: "ss ss-mkm",
+    },
+    plst: {
+        text: "The List",
+        symbol: "ms ms-planeswalker",
+    },
+    spg: {
+        text: "Special Guest",
+        symbol: "ss ss-spg",
+    },
+    otj: {
+        text: "Outlaws of Thunder Junction",
+        symbol: "ss ss-otj",
+    },
+    big: {
+        text: "The Big Score",
+        symbol: "ss ss-big",
+    },
+    otp: {
+        text: "Breaking News",
+        symbol: "ss ss-otp",
+    },
+    mh3: {
+        text: "Modern Horizons 3",
+        symbol: "ss ss-mh3",
+    },
+    m3c: {
+        text: "Modern Horizons 3 Commander",
+        symbol: "ss ss-m3c",
+    },
+    blb: {
+        text: "Bloomburrow",
+        symbol: "ss ss-blb",
+    },
+};
 
 const SetsPicker = ({ hashParams, onUpdate, options }) => (
-  <>
-    <h2>Sets</h2>
-    <fieldset className="checkbox-picker__options sets">
-      {options.sets.map((setAbbr) => (
-        <div key={setAbbr} className="checkbox-picker__option set">
-          <input 
-            className="checkbox-picker__option--checkbox"
-            id={setAbbr}
-            name={setAbbr}
-            value={setAbbr}
-            type="checkbox"
-            checked={hashParams.sets?.includes(setAbbr)}
-            onChange={(e) => onUpdate({
-              sets: hashParams.sets?.includes(e.target.value) ? (
-                hashParams.sets.filter((s) => s !== e.target.value)
-              ) : (
-                [ ...(hashParams.sets || []), e.target.value ]
-              )
-            })} 
-          />
-          <label className="checkbox-picker__option--label" htmlFor={setAbbr}>
-            <i className={SET_DETAILS[setAbbr]?.symbol || 'ms ms-planeswalker'} />
-            {SET_DETAILS[setAbbr]?.text || setAbbr.toUpperCase()}
-            <span className='set-abbr'>({setAbbr})</span>
-          </label>
-        </div>
-      ))}
-    </fieldset>
-  </>
-  )
+    <>
+        <h2>Sets</h2>
+        <fieldset className="checkbox-picker__options sets">
+            {options.sets.map(
+                (setAbbr) =>
+                    setAbbr && (
+                        <div key={setAbbr} className="checkbox-picker__option set">
+                            <input
+                                className="checkbox-picker__option--checkbox"
+                                id={setAbbr}
+                                name={setAbbr}
+                                value={setAbbr}
+                                type="checkbox"
+                                checked={hashParams.sets?.includes(setAbbr)}
+                                onChange={(e) =>
+                                    onUpdate({
+                                        sets: hashParams.sets?.includes(e.target.value)
+                                            ? hashParams.sets.filter((s) => s !== e.target.value)
+                                            : [...(hashParams.sets || []), e.target.value],
+                                    })
+                                }
+                            />
+                            <label className="checkbox-picker__option--label" htmlFor={setAbbr}>
+                                <i className={SET_DETAILS[setAbbr]?.symbol || "ms ms-planeswalker"} />
+                                {SET_DETAILS[setAbbr]?.text || setAbbr.toUpperCase()}
+                                <span className="set-abbr">({setAbbr})</span>
+                            </label>
+                        </div>
+                    )
+            )}
+        </fieldset>
+    </>
+);
 
 export default SetsPicker;
