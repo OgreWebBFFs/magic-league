d0b2e71127dac178ba6dc673349fa31c9226b780
patrickwroach <patrickwroach@gmail.com>
2024-08-03 17:37:06 -0400

    feat: update to secondary buttons

--- app/javascript/components/TradeProposal/TradeProposalButtonLarge.jsx
+++ app/javascript/components/TradeProposal/TradeProposalButtonLarge.jsx
@@ -1,36 +1,32 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
-import Button from '../Button';
-import TradeProposalModal from './TradeProposalModal';
+import React, { useState } from "react";
+import classNames from "classnames";
+import Button from "../Button";
+import TradeProposalModal from "./TradeProposalModal";
 
 const ProposeTrade = () => (
-  <>
-    <i className="fa fa-envelope" aria-hidden="true" />
-    Propose Trade
-  </>
+    <>
+        <i className="fa fa-envelope" aria-hidden="true" />
+        Propose Trade
+    </>
 );
 
 const TradeProposalButtonLarge = ({ isAvailable, card, currentUserId }) => {
-  const [modalOn, setModalOn] = useState(false);
-  return (
-    <>
-      <Button
-        type="button"
-        disabled={!isAvailable}
-        className={classNames('trade-proposal-button', { 'button--inverse': !isAvailable })}
-        onClick={() => setModalOn(true)}
-      >
-        {isAvailable ? <ProposeTrade /> : 'Not in League'}
-      </Button>
-      {modalOn && (
-      <TradeProposalModal
-        card={card}
-        currentUserId={currentUserId}
-        onClose={() => setModalOn(false)}
-      />
-      )}
-    </>
-  );
+    const [modalOn, setModalOn] = useState(false);
+    return (
+        <>
+            <Button
+                type="button"
+                disabled={!isAvailable}
+                className={classNames("trade-proposal-button", { "button--secondary": !isAvailable })}
+                onClick={() => setModalOn(true)}
+            >
+                {isAvailable ? <ProposeTrade /> : "Not in League"}
+            </Button>
+            {modalOn && (
+                <TradeProposalModal card={card} currentUserId={currentUserId} onClose={() => setModalOn(false)} />
+            )}
+        </>
+    );
 };
 
 export default TradeProposalButtonLarge;
