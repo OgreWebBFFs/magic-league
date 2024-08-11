e777db905b6a9b02ce2dd195dd99348a351dd66b
Brandon Seich <blseich@gmail.com>
2024-08-10 22:45:05 -0400

    fix misspelling in bot trade request message

--- app/bot/trade_request.rb
+++ app/bot/trade_request.rb
@@ -7,7 +7,7 @@ class TradeRequest
   def ask_about_card to_user, from_user, card, wishlist_cards
     msg = "#{from_user.name} is interested in trading for your #{card.discord_link}"
     if (wishlist_cards.length > 0)
-      msg += "\n\nThey own the following cards on your wishilst\n"
+      msg += "\n\nThey own the following cards on your wishlist\n"
       msg += wishlist_cards.map {|card| "- #{card.discord_link}"}.join("\n")
     end
     msg += "\n\nYou can message them back by clicking here -> <@#{from_user.discord_id}>"
