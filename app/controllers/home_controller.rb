8f37bdfb84e94b5f9de123759803a67bf8b76ece
Brandon Seich <blseich@gmail.com>
2024-10-12 11:15:11 -0400

    rankings page styling changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
     @match_date = params[:match_date] ? Time.new(params[:match_date][:year].to_i, params[:match_date][:month].to_i, params[:match_date][:day].to_i) : Time.now
 
     @ranked_players = RankingEngine.new(@match_date).generate_rankings
-    @unranked_players = User.all.select{ |u| u.locked_at.nil? && !@ranked_players.any? { |r| r.user === u }}
+    @unranked_players = User.all
+        .select { |u| u.locked_at.nil? && !@ranked_players.any? { |r| r.user === u }}
+        .sort_by { |u| u.name }
   end
 end
