2112fb3cdc87a46cec70394ab02ba1bb6e94ada2
Brandon Seich <blseich@gmail.com>
2024-10-23 20:59:32 -0400

    fix prod webpack config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,4 +1,4 @@
-const webpackConfig = require("./serverClientOrBoth");
+const webpackConfig = require("./config-builder");
 
 const productionEnvOnly = (_clientWebpackConfig, _serverWebpackConfig) => {
     // place any code here that is for production only
