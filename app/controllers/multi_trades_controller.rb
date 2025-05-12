e9b759daa15d99d770efb13272a9b458e3e9d556
Brandon Seich <blseich@gmail.com>
2025-05-11 22:49:03 -0400

    approve and reject working

--- app/controllers/multi_trades_controller.rb
+++ app/controllers/multi_trades_controller.rb
@@ -11,6 +11,15 @@ class MultiTradesController < ApplicationController
     end
   
     def update
+        trade = Trade.find_by(id: params[:id])
+        
+        if params[:status] == 'rejected'
+          flash[:notice] = "You've successfully declined a trade"
+          trade.reject
+        elsif params[:status] == 'approved'
+          flash[:success] = "You've successfully accepted a trade!"
+          trade.approve params[:user_id]
+        end
         render json: {status: 'updated'}
     end
   
@@ -23,66 +32,7 @@ class MultiTradesController < ApplicationController
     end
   
     private
-  
-    def give_cards(card_ids, receive_user, give_user)
-      card_ids.each { |card_id| 
-        card = Card.find_by_id(card_id)
-        if card.received_trades_to_update != 'common'
-          tracked_trade = receive_user.received_trades.find_or_initialize_by(rarity: card.received_trades_to_update)
-          tracked_trade.increment
-        end
-        receive_user.add_card card
-        give_user.remove_card card
-      }
-    end
-  
-    def validate_trade(to, from)
-      invalid_cards = []
-      invalid_cards.concat find_invalid_inventory(from[:cards], from[:id])
-      invalid_cards.concat find_invalid_inventory(to[:cards], to[:id])
-      invalid_cards.concat find_invalid_rarities(to[:cards], from[:id])
-      invalid_cards.concat find_invalid_rarities(from[:cards], to[:id])
-      invalid_cards
-    end
-  
-    def find_invalid_inventory(card_ids, user_id)
-      user = User.find_by_id(user_id)
-      invalid_cards = []
-      card_ids.uniq.each do |cardId|
-        card_to_check = Card.find_by_id(cardId)
-        amnt_in_trade = card_ids.count { |id| id == cardId }
-  
-        has_sufficient_inventory = user.card_inventory(cardId) >= amnt_in_trade
-        
-        if !has_sufficient_inventory
-          invalid_cards.push({:card => card_to_check.name, :player => user.name, :reason => "insufficient inventory"})
-        end    
-      end
-      invalid_cards
-    end
-  
-    def find_invalid_rarities(card_ids, user_id)
-      user = User.find_by_id(user_id)
-      invalid_cards = []
-      available_trades = {
-        :common => 1.0 / 0,# this represents Infinity
-        :uncommon => user.available_trades_for_rarity('uncommon'),
-        :rare => user.available_trades_for_rarity('rare'),
-      }
-      card_ids.uniq.each do |cardId|
-        card_to_check = Card.find_by_id(cardId)
-        rarity_to_check = card_to_check.received_trades_to_update.to_sym
-        amnt_in_trade = card_ids.count { |id| id == cardId }
-  
-        available_trades.update({rarity_to_check => available_trades[rarity_to_check] - amnt_in_trade})
-  
-        if available_trades[rarity_to_check] < 0
-          invalid_cards.push({:card => card_to_check.name, :player => user.name, :reason => "insufficient #{rarity_to_check.to_s} trades"})
-        end
-      end
-      invalid_cards
-    end
-  
+
     def trade_params
         params.require(:data).map do |entry| 
             entry.permit(:user, gives: [], receives: [])
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -6,13 +6,37 @@ class Trade < ApplicationRecord
     User.where(id: exchanges.pluck(:from_user_id, :to_user_id).flatten.uniq)
   end
 
-  private
-  
-  def to_cards
-    self.exchanges.where('user_id = ?', self.to.id).map{ |exch| exch.card }
+  def approve user_id
+    return if status == 'approved'
+    approvals = (status || '').split('|').map(&:to_i)
+    return if approvals.include?(user_id)
+    approvals << user_id
+    if all_participants_approved?(approvals)
+      execute_trade
+      self.status = 'approved'
+      # OgreBot.instance.trade_alerts.trade_accepted trade
+    else
+      self.status = approvals.join('|')
+    end
+    save!
   end
 
-  def from_cards
-    self.exchanges.where('user_id = ?', self.from.id).map{ |exch| exch.card }
+  def reject
+    self.status = 'rejected'
+    # OgreBot.instance.trade_alerts.trade_rejected trade
+    save!
+  end
+
+  private
+
+  def all_participants_approved?(approvals)
+    user_ids = exchanges.flat_map { |e| [e.from_user_id, e.to_user_id] }.uniq
+    (user_ids - approvals).empty?
+  end
+
+  def execute_trade
+    exchanges.each do |exchange|
+      exchange.execute        
+    end
   end
 end
