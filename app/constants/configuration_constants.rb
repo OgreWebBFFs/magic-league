62a5567a16b7cb28e122d0b8cc30d2d52e7b1350
Joe Handzel <joseph.handzel@gmail.com>
2026-04-24 17:17:33 -0400

    configuration_constants - season update to add sos and soa

--- app/constants/configuration_constants.rb
+++ app/constants/configuration_constants.rb
@@ -1,6 +1,8 @@
 module ConfigurationConstants
   VALID_SETS = [
   { code: 'ECL', name: 'Lorwyn Eclipsed', symbol: 'ss ss-ecl' },
+  { code: 'SOS', name: 'Secrets of Strixhaven', symbol: 'ss ss-sos' },
+  { code: 'SOA', name: 'Mystical Archive', symbol: 'ss ss-soa' },
   { code: 'SPG', name: 'Special Guests', symbol: 'ss ss-spg' }
 ].freeze
 .freeze
