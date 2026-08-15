36106a60c187fd44b4f79e7286f4fcef73584966
Joe Handzel <joseph.handzel@gmail.com>
2026-08-15 13:10:39 -0400

    configuration_constants - Aug 2026 new season update of valid sets

--- app/constants/configuration_constants.rb
+++ app/constants/configuration_constants.rb
@@ -1,9 +1,6 @@
 module ConfigurationConstants
   VALID_SETS = [
-  { code: 'ECL', name: 'Lorwyn Eclipsed', symbol: 'ss ss-ecl' },
-  { code: 'SOS', name: 'Secrets of Strixhaven', symbol: 'ss ss-sos' },
-  { code: 'SOA', name: 'Mystical Archive', symbol: 'ss ss-soa' },
-  { code: 'SPG', name: 'Special Guests', symbol: 'ss ss-spg' }
+  { code: 'HOB', name: 'The Hobbit', symbol: 'ss ss-hob' },
 ].freeze
 .freeze
 end
